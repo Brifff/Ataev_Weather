68551d012b11d6fe362491e8700609bf7dd1632d
Ataev.m.N <104029906+Brifff@users.noreply.github.com>
2023-03-20 11:55:23 +0300

    Ataev init

---
+++ Ataev Weather/Ataev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ataev
+//
+//  Created by Атаев Магарам Никматулаевич on 13/2/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
